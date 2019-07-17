64e7231711a8f127720184699fe0fe8c33591374
sdewalt <50034810+sdewalt@users.noreply.github.com>
2019-07-17 14:01:09 -0700

    Add files via upload

---
+++ ConsoleAppAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string input = (Console.ReadLine());
+            int userInput = Convert.ToInt32(input);
+            Console.WriteLine(userInput * 50);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number:");
+            string input = (Console.ReadLine());
+            int userInput = Convert.ToInt32(input);
+            Console.WriteLine(userInput + 25);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number:");
+            string input = (Console.ReadLine());
+            int userInput = Convert.ToInt32(input);
+            Console.WriteLine(userInput / 21.5);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number:");
+            string input = (Console.ReadLine());
+            int userInput = Convert.ToInt32(input);
+            bool trueOrFalse = (userInput > 50);
+            Console.Write(trueOrFalse.ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number:");
+            string input = (Console.ReadLine());
+            int userInput = Convert.ToInt32(input);
+            Console.WriteLine(userInput % 7);
+            Console.ReadLine();
+        }
+    }
+}
