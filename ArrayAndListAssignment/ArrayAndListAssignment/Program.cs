a89dc0ce801016f4b87e7717709f604cf1ac6085
Steven DeWalt <sdewalt3@gmail.com>
2019-07-19 14:28:50 -0700

    Test

---
+++ ArrayAndListAssignment/ArrayAndListAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArrayAndListAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Create an array of integers. Ask the user to select an index of the 
+            //array and then display the integer at that index on the screen.
+            
+           
+
+        }
+    }
+}
