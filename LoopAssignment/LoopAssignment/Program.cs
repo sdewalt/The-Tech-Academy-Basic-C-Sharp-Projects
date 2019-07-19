7de5496295b3d9a7635b5a3399c76b29d957aca7
Steven DeWalt <sdewalt3@gmail.com>
2019-07-19 14:20:18 -0700

    uploaded loop and string assignments

---
+++ LoopAssignment/LoopAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // boolean comparison using a while then modifying to a do while statement
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool guessedInput = number == 95;
+
+            do
+            {
+                switch (number)
+                {
+                    case 23:
+                        Console.WriteLine("You guessed 23. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 47:
+                        Console.WriteLine("You guessed 47. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 79:
+                        Console.WriteLine("You guessed 79. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 95:
+                        Console.WriteLine("You guessed 95. That is correct!");
+                        guessedInput = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!guessedInput);
+
+            Console.Read();
+        }
+    }
+}
---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Convert 3 strings - String.Concat method
+            string fName = "John ";
+            string mName = "Alfred ";
+            string lName = "Smith";
+            string Name = string.Concat(fName, mName, lName);
+            Console.WriteLine(Name);
+            Console.ReadLine();
+
+            ////Convert a string to uppercase
+            string warning = "the sun is really hot!";
+            warning = warning.ToUpper();
+            Console.WriteLine(warning);
+            Console.ReadLine();
+
+            // Create a StringBuilder and build a paragraph of text, one sentance at a time
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("\t Once upon a time in a land far, far away.");
+            sb.AppendLine("Someone needed to write a paragraph using StringBuilder.");
+            sb.AppendLine("They decided to do some research and find some possible solutions.");
+            sb.AppendLine("Now they can write more efficiently in Visual Studio!");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+        }
+    }
+}
