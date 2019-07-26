ec5132dbc0c18ad1bfb6b4cb7dfdd5afc5dabec4
sdewalt <50034810+sdewalt@users.noreply.github.com>
2019-07-26 15:19:12 -0700

    Add files via upload

---
+++ ClassAndMethodsAssignment/ClassAndMethodsAssignment/MathOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsAssignment
+{
+    public class MathOperations
+    {
+       
+
+        public int Add(int num50, int userInput)
+        {
+            int results1 = 50 + userInput;
+            
+            return results1;
+        }
+        public int Subtract(int num50, int userInput)
+        {
+            int results2 = 50 - userInput;
+
+            return results2;
+        }
+        public int Multiply(int num50, int userInput)
+        {
+            int results3 = 50 * userInput;
+
+            return results3;
+        }
+    }
+        
+    
+}
