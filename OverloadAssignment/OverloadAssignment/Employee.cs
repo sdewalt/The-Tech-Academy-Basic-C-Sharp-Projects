9c05aa0a24d9204dab9e64c2709328f74153d4e9
Steven DeWalt <sdewalt3@gmail.com>
2019-08-07 15:04:46 -0700

    overload assignment submission

---
+++ OverloadAssignment/OverloadAssignment/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadAssignment
+{
+    public class Employee
+    {
+        //Property for Id
+        public int Id{ get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (emp1.Id == emp2.Id) ;
+            {
+                return true;
+                Console.WriteLine("The employee Id's are the same.");
+            }
+        }
+        public static bool operator!=(Employee emp1, Employee emp2)
+        {
+            if (emp1.Id != emp2.Id) ;
+            {
+                return false;
+                Console.WriteLine("The employee Id's are different.");    
+
+            }
+        }
+
+        }
+    }
---
+++ OverloadAssignment/OverloadAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadAssignment
+//Overload the "==" operator so it checks if two Employee objects are equal
+//by comparing their Id property.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee empId = new Employee() { Id = 4 };
+            Employee empId2 = new Employee() { Id = 49};
+            Employee empId3 = new Employee() { Id = 49 };
+
+            Console.WriteLine(empId2 == empId);
+            Console.WriteLine(empId3 != empId);
+            Console.Read();
+
+
+                  
+        }
+        
+    }
+}
