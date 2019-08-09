282f8197f30ee966768ae94eb6ef9aaad10d4788
Steven DeWalt <sdewalt3@gmail.com>
2019-08-09 15:15:14 -0700

    struct assignment submission

---
+++ struct assignment/struct assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_assignment
+//Create a struct called Number and give it the property "Amount" 
+//and have it be of data type decimal.
+
+//In the Main() method, create an object of data type Number and
+//assign an amount to it.
+
+//Print the amount to the console.
+{
+    public struct Number
+    {
+        
+        public decimal Amount;
+
+        public double num { get; set; }
+
+        static void Main(string[] args)
+
+        {
+            Number num = new Number() { num = 470.00 };
+            Console.WriteLine(num.num);
+            Console.ReadLine();
+        }
+        
+    }
+}
