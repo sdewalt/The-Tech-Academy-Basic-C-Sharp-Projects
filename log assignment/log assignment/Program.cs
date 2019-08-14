64a7bbccbce5d4bbf87fffd0c3c968140d6fdcdf
Steven DeWalt <sdewalt3@gmail.com>
2019-08-14 13:11:19 -0700

    log assignment submission

---
+++ log assignment/log assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace log_assignment
+//Ask the user for a number.
+
+//Log that number to a text file.
+
+//Print the text file back to the user.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int intVal;
+
+            Console.WriteLine("Please enter a number: ");
+            userInput = Console.ReadLine();
+            intVal = Convert.ToInt32(userInput);
+            Console.WriteLine("intVal: " +intVal);
+
+            string text1 = intVal.ToString();
+            File.WriteAllText(@"C:\Users\Administrator\Logs\log.txt", text1);
+
+            string text2 = File.ReadAllText(@"C:\Users\Administrator\Logs\log.txt");
+            Console.WriteLine("text2: "+text2);
+            Console.ReadLine();
+            
+        }
+    }
+}
